8716b66af8ceecbaf12091ce7a4cfba12ef19516
Karl Wolf <karl.wolf2706@gmail.com>
2022-11-25 10:24:10 +0100

    aufgabe 2 abgegeben und aufgabe3 fast fertig

--- modsim_ub2/nullstellen.c
+++ modsim_ub2/nullstellen.c
@@ -9,6 +9,7 @@
 #endif
 
 // f1(x) = x^3 - 2*x + 2
+// f'(x) = 3x^2 -2
 
 double f1(double x)
 {
@@ -17,63 +18,92 @@ double f1(double x)
 
 double df1(double x)
 {
+  return 3 * pow(x, 2) - 2;
 }
+
+// sin(x*PI / 2) = cos(x*PI)*e^(-3*x)
+// sin(PI/2) = 1
 
 double f2(double x)
 {
+  return cos(x * M_PI) * exp((-3 * x)) - sin((x * M_PI) / 2);
 }
 
 double df2(double x)
 {
+  double h = 0.00001;
+
+  return (f2(x + h) - f2(x-h)) / (2*h);
+  //return 0.5 * exp(-3 * x) * (2 * M_PI * sin(M_PI * x) + M_PI * exp(3 * x) * cos((M_PI * x) / 2) + 6 * cos(M_PI * x));
 }
 
 double Bisektion(double (*f)(double), double xa, double xb, int n)
 {
-  double m;
-
   // Kopfzeile für die Ausgabe
   //  [-3, -0.5] [xa, xb]
 
   // Genauigkeit: 10^-12
 
-  double fx1 = f1(xa);
-  double fx2 = f1(xb);
+  double fx1 = f(xa);
+  double fx2 = f(xb);
 
   double x3 = 0.5 * (xa + xb);
-  double fx3 = f1(x3);
+  double fx3 = f(x3);
   printf("  n:  l                 r                  f(l)          f(r)\n");
   printf("  %d:  %0.5lf          %0.5lf           %0.5lf       %0.5f \n", n, xa, xb, fx1, fx2);
 
-  if (fabs(xb - xa) <= pow(10, -12))
+  if (fabs(xb - xa) <= pow(10, -12) || fabs(fx3) <= pow(10,-12))
   {
     return x3;
   }
 
   if (fx3 * fx2 <= 0)
   {
-    return Bisektion(f1, x3, xb, n + 1);
+    return Bisektion(f, x3, xb, n + 1);
   }
   else
   {
-    return Bisektion(f1, xa, x3, n + 1);
+    return Bisektion(f, xa, x3, n + 1);
   }
 }
 
-double Sekanten(double (*f)(double), double x0, double x1)
+double Sekanten(double (*f)(double), double x0, double x1, int n)
 {
   // x0 = -3 & x1 = -0.5 & abbruch = 10^-12
-  double x_2 = x0 - f1(x0) * ((x1 - x0) / (f1(x1) - f1(x0)));
+  double x2 = x0 - f(x0) * ((x1 - x0) / (f(x1) - f(x0)));
 
-  if (fabs(x_2 - x1) < pow(10, -12))
+  printf("  n:  l                 r                  f(l)          f(r)\n");
+  printf("  %d:  %0.5lf          %0.5lf           %0.5lf       %0.5f \n", n, x0, x1, f(x0), f(x1));
+
+  if (fabs(x2 - x1) < pow(10, -12))
   {
-    return x_2;
+    return x2;
   }
 
-  return Sekanten(f1, x_2, x1);
+  return Sekanten(f, x1, x2, n + 1);
 }
 
-double Newton(double (*f)(double), double (*dfdx)(double), double x0)
+// -1 anstatt -0.5 als startwert liefert ergebnis
+// mit startwert -0.5 wird n >= 100
+double Newton(double (*f)(double), double (*dfdx)(double), double x0, int n)
 {
+  if (n >= 100)
+  {
+    return NAN;
+  }
+  double x1 = x0 - (f(x0) / dfdx(x0));
+
+  printf("  n:  x0                 x1                  f(x0)          f'(x0)\n");
+  printf("  %d:  %0.5lf          %0.5lf           %0.5lf       %0.5f \n", n, x0, x1, f(x0), dfdx(x0));
+
+  if (fabs(x1 - x0) < pow(10, -12))
+  {
+    return x1;
+  }
+  else
+  {
+    return Newton(f, dfdx, x1, n + 1);
+  }
 }
 
 int main()
@@ -85,12 +115,26 @@ int main()
   printf("Nullstelle Bisektion: %.14lf\n", result);
 
   printf("\n\nSekanten\n––––––––\n");
-  result = Sekanten(f1, -3.0, -0.5);
+  result = Sekanten(f1, -3.0, -0.5, 1);
   printf("Nullstelle Sekanten: %.14lf\n", result);
 
   printf("\n\nNewton\n––––––\n");
-  result = Newton(f1, df1, -0.5);
+  result = Newton(f1, df1, -0.5, 1);
   printf("Nullstelle: %.14lf\n", result);
+
+  printf("\n\n ----------------------------");
+
+  printf("\n\nBisektionsverfahren\n––––––\n");
+  result = Bisektion(f2, 0, 3, 1);
+  printf("Nullstelle: %.14lf\n", result);
+
+  printf("\n\nSekanten\n––––––––\n");
+  result = Sekanten(f2, 0, 1, 1);
+  printf("Nullstelle Sekanten: %.14lf\n", result);
+
+  printf("\n\nNewton\n––––––––\n");
+  result = Newton(f2, df2, 0.9, 1);
+  printf("Nullstelle Newton: %.14lf\n", result);
 
   return 0;
 }
--- modsim_ub3/ausgleichsproblem.c
+++ modsim_ub3/ausgleichsproblem.c
@@ -6,11 +6,10 @@
 /*                                                                   */
 /* Es liegen Messdaten "input.dat" in Form von Wertepaaren vor.      */
 /* Durch diese Punkte muss eine Ausgleichskurve gelegt werden.       */
-/*                                                                   */ 
+/*                                                                   */
 /*********************************************************************/
 /*             Zu Beginn einige Hilfsfunktionen                      */
 // --------------------------------------------------------------------
-
 
 /*
 ------- AT*A ---------
@@ -29,17 +28,20 @@ b:    1.769836e-01
 
 /** @brief Funktion um die Wertepaare abzuzählen.
  */
-int getNumberofPoints(char *name) {
-  FILE   *fp;
-  char   *line = NULL;
-  size_t  len = 0;
+int getNumberofPoints(char *name)
+{
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
 
-  if ((fp = fopen(name, "r")) == NULL) {
+  if ((fp = fopen(name, "r")) == NULL)
+  {
     exit(EXIT_FAILURE);
   }
 
   int cnt = 0;
-  while (getline(&line, &len, fp) != -1) {
+  while (getline(&line, &len, fp) != -1)
+  {
     cnt++;
   }
 
@@ -50,17 +52,20 @@ int getNumberofPoints(char *name) {
 }
 /** @brief In dieser Funktion werden die Wertepaare eingelesen und in Form von Arrays t[N] und I[N] übergeben.
  */
-void readFile(char *name, int t[], double I[]) {
-  FILE   *fp;
-  char   *line = NULL;
-  size_t  len = 0;
+void readFile(char *name, int t[], double I[])
+{
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
 
-  if ((fp = fopen(name, "r")) == NULL) {
+  if ((fp = fopen(name, "r")) == NULL)
+  {
     exit(EXIT_FAILURE);
   }
 
   int cnt = 0;
-  while (getline(&line, &len, fp) != -1) {
+  while (getline(&line, &len, fp) != -1)
+  {
     sscanf(line, "%i %lf", &t[cnt], &I[cnt]);
     cnt++;
   }
@@ -72,12 +77,13 @@ void readFile(char *name, int t[], double I[]) {
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
   // Abzaehlen der Wertepaare
   int N = getNumberofPoints("input.dat");
 
-  int    t[N]; //Vektor für den Kalenderwoche der Messung
-  double I[N]; //Vektor für den gemessenen Inzidenzwerte
+  int t[N];    // Vektor für den Kalenderwoche der Messung
+  double I[N]; // Vektor für den gemessenen Inzidenzwerte
   // Einlesen der Daten
   readFile("input.dat", t, I);
 
@@ -86,35 +92,116 @@ int main(int argc, char* argv[]){
 
   double logI[N];
 
-//_____________________________________________________________________
-// Berechnung der Matrix A^T*A und der rechten Seiten A^T*y
+  for (int x = 0; x < N; x++)
+  {
+    logI[x] = log(I[x]);
+  }
+
+  //_____________________________________________________________________
+  // Berechnung der Matrix A^T*A und der rechten Seiten A^T*y
   double ATA[2][2] = {{0.0, 0.0},
                       {0.0, 0.0}};
+  double ATA_INV[2][2] = {{0.0, 0.0},
+                          {0.0, 0.0}};
   double ATlogI[2] = {0.0, 0.0};
 
+  double A[2][N - 1];
+  double AT[N - 1][2];
+
+  for (int j = 0; j < N - 1; j++)
+  {
+    A[0][j] = t[j] - t_0;
+    A[1][j] = 1;
+
+    AT[j][1] = 1;
+    AT[j][0] = t[j] - t_0;
+  }
+
+  double ATlogI1 = 0.0;
+  double ATlogI0 = 0.0;
+  for (int x = 0; x < N; x++)
+  {
+    ATlogI0 = ATlogI0 + (logI[x] * AT[x][0]);
+    ATlogI1 = ATlogI1 + (logI[x] * 1);
+  }
+  ATlogI[0] = ATlogI0;
+  ATlogI[1] = ATlogI1;
+
+  for (int x = 0; x < 2; x++)
+  {
+    for (int y = 0; y < 2; y++)
+    {
+      double temp = 0;
+      for (int q = 0; q < N - 1; q++)
+      {
+        temp = temp + (AT[q][y] * A[x][q]);
+      }
+      ATA[x][y] = temp;
+    }
+  }
+  printf("A Matrix: \n");
+  for (int q = 0; q < N - 1; q++)
+  {
+    printf("%.5lf %.5lf \n", A[0][q], A[1][q]);
+  }
+
+  printf("AT Matrix: \n");
+  for (int q = 0; q < N - 1; q++)
+  {
+    printf("%.5lf ", AT[q][0]);
+  }
+  printf("\n");
+  for (int q = 0; q < N - 1; q++)
+  {
+    printf("%.5lf ", AT[q][1]);
+  }
+
+  printf("\nATA Matrix: \n");
+  printf("%.5lf %.5lf \n", ATA[0][0], ATA[0][1]);
+  printf("%.5lf %.5lf \n", ATA[1][0], ATA[1][1]);
 
   // Invertierung von ATA (wegen kleiner Groesse)
+  double det = ATA[0][0] * ATA[1][1] - ATA[1][0] * ATA[0][1];
+  ATA_INV[0][0] = ATA[1][1] / det;      // a
+  ATA_INV[1][0] = ATA[1][0] / det * -1; // b
+  ATA_INV[0][1] = ATA[0][1] / det * -1; // c
+  ATA_INV[1][1] = ATA[0][0] / det;      // d
+
+  printf("\nATA Inv Matrix: \n");
+  printf("%.5lf %.5lf \n", ATA_INV[0][0], ATA_INV[0][1]);
+  printf("%.5lf %.5lf \n", ATA_INV[1][0], ATA_INV[1][1]);
 
   // Ermittlung von ln(a) und b aus ATA^{-1} und ATy
 
+  // ATA * delta = ATlogI
+  // delta = (ln(a),b)^T
+  //  (ln(a))    (ATAlogI[0]) (ATA_INV[0][0] ATA_INV[0][1])
+  //           =
+  //  ( b   )    (ATAlogI[0]) (ATA_INV[1][0] ATA_INV[1][1])
+
+  double ln_a = ATA_INV[0][0] * ATlogI[0] + ATA_INV[1][0] * ATlogI[0];
+  double b = ATA_INV[0][1] * ATlogI[1] + ATA_INV[1][1] * ATlogI[1];
 
   // Bestimmung von a selbst
   double a = exp(ln_a);
 
-  // Plotten wenn plotflag!=0
-  long plotflag = 0;
+  printf(" a = %.5lf , b = %.5lf", ln_a, b);
 
-  if (plotflag) {
-    FILE *gp = popen("gnuplot -p","w");
-    fprintf(gp,"reset; set key left top box; set xlabel \"t - t_0\";\n"
-        "set ylabel \"y\";\n"
-        "set autoscale fix\n"
-//         "set logscale xy\n"
-        " f(t) = %le*exp(%le*(t - 23));\n "
-        " plot f(x) lt -1 lw 2, \"input.dat\" using 1:2 pt 7 title 'measured data';\n", //lt: LineType, lw: Linewidth, using 1:2: zweite spalte verwenden
-        a, b);
+  // Plotten wenn plotflag!=0
+  long plotflag = 1;
+
+  if (plotflag)
+  {
+    FILE *gp = popen("gnuplot -p", "w");
+    fprintf(gp, "reset; set key left top box; set xlabel \"t - t_0\";\n"
+                "set ylabel \"y\";\n"
+                "set autoscale fix\n"
+                "set logscale xy\n"
+                " f(t) = %le*exp(%le*(t - 23));\n "
+                "set terminal png size 1400,1300; set output 'xyz.png';plot f(x) lt -1 lw 2, \"input.dat\" using 1:2 pt 7 title 'measured data';\n", // lt: LineType, lw: Linewidth, using 1:2: zweite spalte verwenden
+            a, b);
     pclose(gp);
   }
-  
+
   return 0;
 }
